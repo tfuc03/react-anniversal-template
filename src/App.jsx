d454e65073d6a33fefe589204f854e3e10a6a379
Tfuc03 <phamphucdev1n@gmail.com>
2025-10-22 02:33:06 +0700

    fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
     <section>
-      <div className='flex flex-col bg-[linear-gradient(120deg,#FFF2E1_0%,#FFE3E3_40%,#E8EAF6_100%)] w-full p-10 h-screen md:gap-y-10 lg:gap-y-20'>
+      <div className='flex flex-col bg-[linear-gradient(120deg,#FFF2E1_0%,#FFE3E3_40%,#E8EAF6_100%)] w-full p-10 lg:h-screen md:gap-y-10 lg:gap-y-20'>
         <Header/>
         
       </div>
