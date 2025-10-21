34ae7f93a2a3ed1ae3304040d99ab21bf165199b
Tfuc03 <phamphucdev1n@gmail.com>
2025-10-22 02:31:23 +0700

    fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,17 @@ import ReCap from './layout/ReCap'
 
 function App() {
 
+  
+
   return (
     <>
     <section>
       <div className='flex flex-col bg-[linear-gradient(120deg,#FFF2E1_0%,#FFE3E3_40%,#E8EAF6_100%)] w-full p-10 h-screen md:gap-y-10 lg:gap-y-20'>
-        <Header onScrollClick={handleScrollToStory}/>
+        <Header/>
         
       </div>
       <div>
-        <FirstStory ref={firstStoryRef}/>
+        <FirstStory />
       </div>
       <div>
         <FirstAdventure />
