08253052fa6487dc12bd71aa47cfcdc22f2de1f6
Tfuc03 <phamphucdev1n@gmail.com>
2025-10-23 07:48:21 +0700

    Final

--- src/components/CircularGallery.jsx
+++ src/components/CircularGallery.jsx
@@ -332,18 +332,14 @@ class App {
   }
   createMedias(items, bend = 1, textColor, borderRadius, font) {
     const defaultItems = [
-      { image: `https://picsum.photos/seed/1/800/600?grayscale`, text: 'Bridge' },
-      { image: `https://picsum.photos/seed/2/800/600?grayscale`, text: 'Desk Setup' },
-      { image: `https://picsum.photos/seed/3/800/600?grayscale`, text: 'Waterfall' },
-      { image: `https://picsum.photos/seed/4/800/600?grayscale`, text: 'Strawberries' },
-      { image: `https://picsum.photos/seed/5/800/600?grayscale`, text: 'Deep Diving' },
-      { image: `https://picsum.photos/seed/16/800/600?grayscale`, text: 'Train Track' },
-      { image: `https://picsum.photos/seed/17/800/600?grayscale`, text: 'Santorini' },
-      { image: `https://picsum.photos/seed/8/800/600?grayscale`, text: 'Blurry Lights' },
-      { image: `https://picsum.photos/seed/9/800/600?grayscale`, text: 'New York' },
-      { image: `https://picsum.photos/seed/10/800/600?grayscale`, text: 'Good Boy' },
-      { image: `https://picsum.photos/seed/21/800/600?grayscale`, text: 'Coastline' },
-      { image: `https://picsum.photos/seed/12/800/600?grayscale`, text: 'Palm Trees' }
+      { image: `/images/photo.JPG`, text: '' },
+      { image: `/images/photo8.JPG`, text: '' },
+      { image: `/images/photo11.JPG`, text: '' },
+      { image: `/images/photo37.JPG`, text: '' },
+      { image: `/images/photo22.JPG`, text: '' },
+      { image: `/images/photo31.JPG`, text: '' },
+      { image: `/images/photo12.JPG`, text: '' },
+      { image: `/images/photo40.JPG`, text: '' },
     ];
     const galleryItems = items && items.length ? items : defaultItems;
     this.mediasImages = galleryItems.concat(galleryItems);
